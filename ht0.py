3ef6e0d3203aaf93e6324ed9885cb8fa69bd5c27
Angel Marroquín <3668270220101@ingenieria.usac.edu.gt>
2021-04-15 15:01:03 -0600

    Hoja de Trabajo 0

---
+++ ht0.py
@@ -0,0 +1,13 @@
+#ingresando la masa corporal en kg y la altura en m
+print("ingrese su masa corporal en kg y su altura en m")
+masa = input("introduzca un su masa ")
+altura = input("introduzca su altura ")
+
+#calculando el imc
+imc = masa / (altura**2)
+
+#redondeando a 2 decimales
+imc2decimales = round(imc, 2)
+
+#imprimiendo el imc redondeado
+print("Tu índice de masa corporal es " , imc2decimales)
